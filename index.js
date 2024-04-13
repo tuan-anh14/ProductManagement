b0589bafffbc6b26ca1026be847eebe33551afd5
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-04-13 16:58:42 +0700

    Cài đặt Mongoose

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const path = require("path");
 const route = require("./routes/client/index.route");
+const database = require("./config/database");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.static("public"));
+
+database.connect();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
