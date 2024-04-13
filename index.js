49b657a7b7a36147f9d8b788de622fe997531b70
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-04-13 15:27:17 +0700

    Nhúng bootstrap 4 - file tĩnh

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const express = require('express')
-const path = require("path")
-const route = require("./routes/client/index.route")
-require('dotenv').config()
+const express = require("express");
+const path = require("path");
+const route = require("./routes/client/index.route");
+require("dotenv").config();
 
-const app = express()
-const port = process.env.PORT
+const app = express();
+const port = process.env.PORT;
 
-app.set("views", path.join(__dirname, "views"))
+app.use(express.static("public"));
+
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 // Route
-route(app)
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
