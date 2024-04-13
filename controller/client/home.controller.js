87408e8b5cc7de4f799652a864d63c1318c64ae9
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-04-13 15:15:15 +0700

    Cấu trúc lại folder PUG

--- controller/client/home.controller.js
+++ controller/client/home.controller.js
@@ -1,4 +1,6 @@
 
 module.exports.index = (req, res) => {
-    res.render("client/pages/home/index")
+    res.render("client/pages/home/index", {
+        pageTitle: "Trang chủ"
+    })
 }
--- controller/client/product.controller.js
+++ controller/client/product.controller.js
@@ -1,4 +1,6 @@
 
 module.exports.index = (req, res) => {
-    res.render("client/pages/products/index")
+    res.render("client/pages/products/index", {
+        pageTitle: "Danh sách sản phẩm"
+    })
 }
