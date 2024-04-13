5ea92a7090ac42855599318d03cb89fac93fefc3
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-04-13 15:06:49 +0700

    Cấu hình env

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 const express = require('express')
 const path = require("path")
 const route = require("./routes/client/index.route")
+require('dotenv').config()
 
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "pug");
-
-// app.get("/", (req, res) => {
-//   res.render("client/pages/home/index")
-// })
-
-// app.get("/products", (req, res) => {
-//   res.render("client/pages/products/index")
-// })
 
 // Route
 route(app)
