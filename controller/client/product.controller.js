e36d9e17ff8fa1588614cef01efa6aebed426b77
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-04-15 10:02:20 +0700

    Làm trang ds sản phẩm bên Client

--- controller/client/product.controller.js
+++ controller/client/product.controller.js
@@ -1,6 +1,25 @@
+const Product = require("../../model/product.model")
 
-module.exports.index = (req, res) => {
-    res.render("client/pages/products/index", {
-        pageTitle: "Danh sách sản phẩm"
-    })
-}
+module.exports.index = async (req, res) => {
+    try {
+        const products = await Product.find({
+            status: "active",
+            deleted: false
+        });
+
+        const newProducts = products.map(item => ({
+            ...item.toObject(), // Copy all properties of item
+            priceNew: (item.price * (100 - item.discountPercentage) / 100).toFixed(0)
+        }));
+
+        console.log(newProducts);
+
+        res.render("client/pages/products/index", {
+            pageTitle: "Danh sách sản phẩm",
+            products: newProducts
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
+};
