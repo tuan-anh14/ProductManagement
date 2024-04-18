50e99b8a3f124ee251b2a7ff0f6f015604b7653e
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-04-18 21:15:15 +0700

    Tính năng lọc theo trạng thái

--- controller/admin/products.controller.js
+++ controller/admin/products.controller.js
@@ -1,14 +1,51 @@
-const Product = require("../../model/product.model")
+const Product = require("../../model/product.model");
 
 //[GET] /admin/products
-module.exports.products = async(req, res) => {
-    const products = await Product.find({
-        deleted: false
-    })
-    console.log(products)
+module.exports.products = async (req, res) => {
+  // console.log(req.query.status)
+  let filterStatus = [
+    {
+      name: "Tất cả",
+      status: "",
+      class: ""
+    },
 
-    res.render("admin/pages/products/index.pug", {
-        pageTitle: "Trang sản phẩm",
-        products: products
-    })
-}
+    {
+      name: "Hoạt động",
+      status: "active",
+      class: ""
+    },
+
+    {
+      name: "Dừng hoạt động",
+      status: "inactive",
+      class: ""
+    }
+  ]
+
+  if(req.query.status){
+    const index = filterStatus.findIndex( (item) => (item.status == req.query.status))
+    filterStatus[index].class = "active"
+  }else {
+    const index = filterStatus.findIndex( (item) => (item.status == ""))
+    filterStatus[index].class = "active"
+  }
+
+
+  let find = {
+    deleted: false
+  }
+
+  if(req.query.status){
+    find.status = req.query.status
+  }
+
+  const products = await Product.find(find);
+  // console.log(products)
+
+  res.render("admin/pages/products/index.pug", {
+    pageTitle: "Trang danh sách sản phẩm",
+    products: products,
+    filterStatus: filterStatus,
+  });
+};
---
+++ public/admin/js/script.js
@@ -0,0 +1,22 @@
+//Button status
+const buttonsStatus = document.querySelectorAll("[button-status]");
+if (buttonsStatus.length > 0) {
+  let url = new URL(window.location.href);
+  console.log(url)
+  buttonsStatus.forEach((button) => {
+    button.addEventListener("click", () => {
+      const status = button.getAttribute("button-status");
+      
+      if(status) {
+        url.searchParams.set("status", status)
+      }else{
+        url.searchParams.delete("status")
+      }
+
+      console.log(url.href)
+      window.location.href = url.href
+    });
+  });
+}
+
+//End button status
