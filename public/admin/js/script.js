800cd6a67876f1af12c10efc04a1564abc0afa3e
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-04-20 15:08:53 +0700

    Tính năng tìm kiếm (frontend)

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -20,3 +20,23 @@ if (buttonsStatus.length > 0) {
 }
 
 //End button status
+
+//Form Search
+const formSearch = document.querySelector("#form-search")
+if(formSearch){
+  let url = new URL(window.location.href);
+
+  formSearch.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const keyword = e.target.elements.keyword.value
+
+    if(keyword){
+      url.searchParams.set("keyword", keyword)
+    }else{
+      url.searchParams.delete("keyword", keyword)
+    }
+    window.location.href = url.href
+  })
+}
+
+//End Form Search
