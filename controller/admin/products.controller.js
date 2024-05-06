b66b6df7f8215bed7939254df621a580ad2ca0a8
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-05-06 21:19:10 +0700

    Hiển thị thời gian xoá sp

--- controller/admin/products.controller.js
+++ controller/admin/products.controller.js
@@ -98,9 +98,10 @@ module.exports.deleteItem = async (req, res) => {
 module.exports.deleteItem = async (req, res) => {
   const id = req.params.id;
 
-  await Product.updateOne({ _id: id }, {deleted: true});
+  await Product.updateOne({ _id: id }, { 
+    deleted: true,
+    deletedAt: new Date()
+  });
 
   res.redirect("back");
 };
-
-
