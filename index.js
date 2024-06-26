89def0aab3907e296f333cb8761792be56663965
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-06-26 17:23:05 +0700

    config deploy online

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
 
-app.use(express.static("public"));
+app.use(express.static(`${__dirname}/public`));
 
 app.use(methodOverride("_method"));
 
