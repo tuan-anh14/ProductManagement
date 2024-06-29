40bf8e48c1327c730836e1e1160c97383d0422c3
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-06-29 15:43:52 +0700

    Đẩy file tĩnh lên cloud

--- routes/admin/products.route.js
+++ routes/admin/products.route.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const route = express.Router();
 const multer = require("multer");
-const storageMulter = require("../../helpers/storageMulter");
-const upload = multer({ storage: storageMulter() });
+
+
+const upload = multer();
 
 const controller = require("../../controller/admin/products.controller");
-const validate = require("../../validates/admin/product.validate")
+const validate = require("../../validates/admin/product.validate");
+
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware")
 
 route.get("/", controller.products);
 
@@ -17,19 +20,23 @@ route.delete("/delete/:id", controller.deleteItem);
 
 route.get("/create", controller.create);
 
-route.post("/create", 
-    upload.single("thumbnail"), 
-    validate.createPost,
-    controller.createPost);
+route.post(
+  "/create",
+  upload.single("thumbnail"),
+  uploadCloud.upload,
+  validate.createPost,
+  controller.createPost
+);
 
-route.get("/edit/:id", controller.edit);    
+route.get("/edit/:id", controller.edit);
 
 route.patch(
-    "/edit/:id",
-    upload.single("thumbnail"), 
-    validate.createPost,
-    controller.editPatch);   
+  "/edit/:id",
+  upload.single("thumbnail"),
+  validate.createPost,
+  controller.editPatch
+);
 
-route.get("/detail/:id", controller.detail);    
+route.get("/detail/:id", controller.detail);
 
 module.exports = route;
