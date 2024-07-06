0afce2c7a7e97db32400c65d930e2fc435245ace
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-07-06 17:34:31 +0700

    Xd phần danh mục sp (p1)

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,6 +1,7 @@
 
 const dashboardRoutes = require("../admin/dashboard.route")
 const productRoutes = require("../admin/products.route")
+const productCategoryRoutes = require("../admin/products-category.route")
 const systemConfig = require("../../config/system")
 
 module.exports = (app) => {
@@ -9,4 +10,6 @@ module.exports = (app) => {
     app.use(PATH_ADMIN + "/dashboard", dashboardRoutes)
 
     app.use(PATH_ADMIN + "/products", productRoutes)
+
+    app.use(PATH_ADMIN + "/products-category", productCategoryRoutes)
 }
---
+++ routes/admin/products-category.route.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const route = express.Router();
+const multer = require("multer");
+
+const upload = multer();
+
+const controller = require("../../controller/admin/products-category.controller");
+const validate = require("../../validates/admin/product-category.validate");
+
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware")
+
+route.get("/", controller.index);
+
+route.get("/create", controller.create);
+route.post(
+    "/create",
+    upload.single("thumbnail"),
+    uploadCloud.upload,
+    validate.createPost,
+    controller.createPost
+  );
+
+module.exports = route;
