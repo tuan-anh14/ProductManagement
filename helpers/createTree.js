704b0a13ebfd76452a66bedb38d11be71ad9a320
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-07-08 16:53:49 +0700

    fix lỗi STT

--- helpers/createTree.js
+++ helpers/createTree.js
@@ -1,20 +1,25 @@
+let count = 0;
+
 const createTree = (arr, parentId = "") => {
-    const tree = [];
-    
-    arr.forEach((item) => {
-      if (item.parent_id === parentId) {
-        const newItem = item;
-        const children = createTree(arr, item.id);
-        if (children.length > 0) {
-          newItem.children = children;
-        }
-        tree.push(newItem);
+  const tree = [];
+
+  arr.forEach((item) => {
+    if (item.parent_id === parentId) {
+      count++;
+      const newItem = item;
+      newItem.index = count;
+      const children = createTree(arr, item.id);
+      if (children.length > 0) {
+        newItem.children = children;
       }
-    });
-    
-    return tree;
-}
+      tree.push(newItem);
+    }
+  });
+
+  return tree;
+};
 module.exports.tree = (arr, parentId = "") => {
-    const tree = createTree(arr, parentId = "");
-    return tree;
-}
+  count = 0;
+  const tree = createTree(arr, (parentId = ""));
+  return tree;
+};
