580cf01ccec6b5ba1c404de2101d15e88903f5fd
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-07-08 16:28:57 +0700

    Xd trang quản lí danh mục (p2)

--- controller/admin/products-category.controller.js
+++ controller/admin/products-category.controller.js
@@ -1,26 +1,40 @@
 const ProductCategory = require("../../model/product-category.model");
 const systemConfig = require("../../config/system");
+const createTreeHelper = require("../../helpers/createTree") 
 
 //[GET] /admin/products-category
 module.exports.index = async (req, res) => {
   let find = {
     deleted: false,
   };
+
   
   const records = await ProductCategory.find(find)
   
+  const newRecords = createTreeHelper.tree(records)
   
   res.render("admin/pages/products-category/index.pug", {
     pageTitle: "Trang danh mục sản phẩm",
-    records: records
+    records: newRecords
   });
 };
 
 
 //[GET] /admin/products-category/create
 module.exports.create = async (req, res) => {
+  let find = {
+    deleted: false
+  }
+  
+  const records = await ProductCategory.find(find)
+
+  const newRecords = createTreeHelper.tree(records)
+
+  console.log(newRecords)
+
   res.render("admin/pages/products-category/create.pug", {
     pageTitle: "Tạo danh mục sản phẩm",
+    records: newRecords
   });
 };
 
--- validates/admin/product-category.validate.js
+++ validates/admin/product-category.validate.js
@@ -6,8 +6,8 @@ module.exports.createPost = async (req, res, next) => {
       return;
     }
       
-    if (req.body.title.length < 8) {
-      req.flash("error", "Vui lòng nhập độ dài tiêu đề lớn hơn 8 kí tự!");
+    if (req.body.title.length < 2) {
+      req.flash("error", "Vui lòng nhập độ dài tiêu đề lớn hơn 2 kí tự!");
   
       res.redirect("back");
   
