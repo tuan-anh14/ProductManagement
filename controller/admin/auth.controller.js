74980ea516a233b8dcf067f8e268088194c84ed1
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-07-26 10:45:37 +0700

    Thêm middleware cho route private

--- controller/admin/auth.controller.js
+++ controller/admin/auth.controller.js
@@ -4,9 +4,13 @@ const systemConfig = require("../../config/system");
 
 //[GET] /admin/auth/login
 module.exports.login = async (req, res) => {
-  res.render("admin/pages/auth/login.pug", {
-    pageTitle: "Đăng nhập",
-  });
+  if (req.cookies.token) {
+    res.redirect(`${systemConfig.prefixAdmin}/dashboard`);
+  } else {
+    res.render("admin/pages/auth/login.pug", {
+      pageTitle: "Đăng nhập",
+    });
+  }
 };
 
 //[POST] /admin/auth/login
