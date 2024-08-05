0793484d6d2937988b2ff2f0794cf4aa5159626d
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-08-05 11:05:40 +0700

    Hiển thị danh sách sản phẩm mới nhất

--- controller/client/home.controller.js
+++ controller/client/home.controller.js
@@ -11,12 +11,25 @@ module.exports.index = async (req, res) => {
     status: "active",
   }).limit(6);
 
-  const newProducts = productHelpers.priceNewProducts(productsFeatured);
+  const newProductsFeatured = productHelpers.priceNewProducts(productsFeatured);
 
   // Hết lấy sản phẩm nổi bật
 
+  // Hiển thị danh sách sản phẩm mới nhất
+  const productNews = await Product.find({
+    deleted: false,
+    status: "active",
+  })
+    .sort({ position: "desc" })
+    .limit(6);
+
+  const newProductsNew = productHelpers.priceNewProducts(productNews);
+
+  // End Hiển thị danh sách sản phẩm mới nhất
+
   res.render("client/pages/home/index", {
     pageTitle: "Trang chủ",
-    productsFeatured: newProducts,
+    productsFeatured: newProductsFeatured,
+    productsNew: newProductsNew,
   });
 };
