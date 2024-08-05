ec7c36c803e2983b21638e5eb754eeb51e955ec4
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-08-05 22:54:02 +0700

    Trang danh sách sản phẩm bên client

--- controller/client/product.controller.js
+++ controller/client/product.controller.js
@@ -1,6 +1,9 @@
 const Product = require("../../model/product.model");
+const ProductCategory = require("../../model/product-category.model");
 
 const productHelpers = require("../../helpers/product");
+const productCategoryHelpers = require("../../helpers/product-category");
+const { get } = require("mongoose");
 
 //[GET] /products
 module.exports.index = async (req, res) => {
@@ -41,5 +44,43 @@ module.exports.detail = async (req, res) => {
     });
   } catch (error) {
     res.redirect(`/products`);
+  }
+};
+
+//[GET] /products/:slugCategory
+module.exports.category = async (req, res) => {
+  try {
+    console.log(req.params.slugCategory);
+
+    const category = await ProductCategory.findOne({
+      slug: req.params.slugCategory,
+      deleted: false,
+    });
+
+    if (!category) {
+      return res.status(404).send("Category not found");
+    }
+
+    const listSubCategory = await productCategoryHelpers.getSubCategory(category.id);
+
+    const listSubCategoryId = listSubCategory.map((item) => item.id);
+
+    const products = await Product.find({
+      product_category_id: { $in: [category.id, ...listSubCategoryId] },
+      deleted: false,
+      status: "active",
+    }).sort({ position: "desc" });
+
+    const newProducts = productHelpers.priceNewProducts(products);
+
+    console.log(products);
+
+    res.render("client/pages/products/index", {
+      pageTitle: category.title,
+      products: newProducts,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
   }
 };
--- routes/client/product.route.js
+++ routes/client/product.route.js
@@ -5,6 +5,8 @@ const controller = require("../../controller/client/product.controller")
 
 route.get('/', controller.index)
 
-route.get('/:slug', controller.detail)
+route.get('/:slugCategory', controller.category)
+
+// route.get('/:slug', controller.detail)
 
 module.exports = route; 
