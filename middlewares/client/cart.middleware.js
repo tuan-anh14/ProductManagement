5ca673adde0f29266fd0d2ef0a635bd6a3685152
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-08-09 09:40:16 +0700

    Làm nút Mini Cart trên Header

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -11,7 +11,16 @@ module.exports.cartId = async (req, res, next) => {
       expires: new Date(Date.now() + expiresTime),
     });
   } else {
-    // Khi đã có giỏ hàng
+    const cart = await Cart.findOne({
+      _id: req.cookies.cartId,
+    });
+
+    cart.totalQuantity = cart.products.reduce(
+      (sum, item) => sum + item.quantity,
+      0
+    );
+
+    res.locals.miniCart = cart;
   }
 
   next();
