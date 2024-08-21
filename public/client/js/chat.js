130239f27594a075bf127b528a5a02e6572a30e1
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-08-21 11:01:35 +0700

    Chèn nhiều ảnh ( phần 1)

--- public/client/js/chat.js
+++ public/client/js/chat.js
@@ -1,13 +1,25 @@
 import * as Popper from "https://cdn.jsdelivr.net/npm/@popperjs/core@^2/dist/esm/index.js";
 
+// File Upload With Preview
+const upload = new FileUploadWithPreview.FileUploadWithPreview("upload-image", {
+  multiple: true,
+  maxFileCount: 6,
+});
+
+// End File Upload With Preview
+
 // CLIENT_SEND_MESSAGE
 const formSendData = document.querySelector(".chat .inner-form");
 if (formSendData) {
   formSendData.addEventListener("submit", (e) => {
     e.preventDefault();
     const content = e.target.elements.content.value;
+    const images = upload.cachedFileArray || [];
 
-    if (content) {
+    console.log(images)
+
+    if (content || images.length > 0) {
+      // Gửi content hoặc ảnh lên server
       socket.emit("CLIENT_SEND_MESSAGE", content);
       e.target.elements.content.value = "";
       socket.emit("CLIENT_SEND_TYPING", "hidden");
