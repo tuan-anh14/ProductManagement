1e9e6ccb4d4f3f1ed93ca8da4cdee53efde47657
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-08-29 21:24:36 +0700

    Hiển thị A vào danh sách lời mời kb của B ( tính năng chấp nhận )

--- public/client/js/users.js
+++ public/client/js/users.js
@@ -131,6 +131,17 @@ socket.on("SERVER_RETURN_INFO_ACCEPT_FRIEND", (data) => {
       socket.emit("CLIENT_REFUSE_FRIEND", userId);
     });
     // Hết Xoá lời mời kết bạn
+
+    // Chấp nhận lời mời kết bạn
+    const btnAcceptFriend = newBoxUser.querySelector("[btn-accept-friend]");
+    btnAcceptFriend.addEventListener("click", () => {
+      btnAcceptFriend.closest(".box-user").classList.add("accepted");
+
+      const userId = btnAcceptFriend.getAttribute("btn-accept-friend");
+
+      socket.emit("CLIENT_ACCEPT_FRIEND", userId);
+    });
+    // Hết Chấp nhận lời mời kết bạn
   }
 });
 
