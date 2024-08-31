19a7b71ae4323675b6e80c7baa7d783efab062d2
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-08-31 10:56:46 +0700

    Hiển thị danh sách phòng chat

--- controller/client/rooms-chat.controller.js
+++ controller/client/rooms-chat.controller.js
@@ -3,8 +3,17 @@ const RoomChat = require("../../model/room-chat.model");
 
 // [GET] /rooms-chat/
 module.exports.index = async (req, res) => {
+  const userId = res.locals.user.id;
+
+  const listRoomChat = await RoomChat.find({
+    "users.user_id": userId,
+    typeRoom: "group",
+    deleted: false,
+  });
+
   res.render("client/pages/rooms-chat/index", {
     pageTitle: "Danh sách phòng",
+    listRoomChat: listRoomChat,
   });
 };
 
